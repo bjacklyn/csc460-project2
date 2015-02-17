299e372b3180c825b57ea510cb2dcc8b8fe95304
bjacklyn <brandonjacklyn@live.ca>
2015-02-16 17:26:27 -0800

    add project2

---
+++ Project2/Project2/Project2.cpp
@@ -0,0 +1,17 @@
+/*
+ * Project2.cpp
+ *
+ * Created: 2/16/2015 5:14:00 PM
+ *  Author: Bjacklyn
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
